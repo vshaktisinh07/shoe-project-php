f59d6b56f32af5288394e233c2a8b7372bc4ee7a
vshaktisinh07 <shaktisinhv007@gmail.com>
2026-03-12 09:02:35 +0530

    4th commit

--- admin/login.php
+++ admin/login.php
@@ -37,13 +37,44 @@ if (isset($_POST['login'])) {
 
         <form method="POST">
 
-            <input type="text" name="username" placeholder="Username" required>
+            <input type="text" name="username" placeholder="Username">
 
-            <input type="password" name="password" placeholder="Password" required>
+            <input type="password" name="password" placeholder="Password">
 
             <button name="login">Login</button>
 
         </form>
+
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+        <script>
+            $(document).ready(function() {
+                $('form').on('submit', function(e) {
+                    var username = $('input[name="username"]').val().trim();
+                    var password = $('input[name="password"]').val().trim();
+                    var valid = true;
+                    var errorMsg = "";
+
+                    if (username === "") {
+                        errorMsg += "Username is required. ";
+                        valid = false;
+                    }
+                    if (password === "") {
+                        errorMsg += "Password is required.";
+                        valid = false;
+                    }
+
+                    if (!valid) {
+                        e.preventDefault();
+                        if ($('.js-error').length === 0) {
+                            $('<p class="error js-error"></p>').insertAfter('form');
+                        }
+                        $('.js-error').text(errorMsg);
+                    } else {
+                        $('.js-error').remove();
+                    }
+                });
+            });
+        </script>
 
         <?php
         if (isset($error)) {
